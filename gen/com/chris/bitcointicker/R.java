38692343d7fb68b82526c02a41686c0e0e363f3c
chrstopher lee <lee.christopher36@gmail.com>
2014-04-06 23:32:17 -0400

    implemented basic widget that does nothing yet

--- gen/com/chris/bitcointicker/R.java
+++ gen/com/chris/bitcointicker/R.java
@@ -11,22 +11,25 @@ public final class R {
     public static final class attr {
     }
     public static final class color {
-        public static final int grey=0x7f050000;
+        public static final int grey=0x7f060000;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int LinearLayout1=0x7f070000;
-        public static final int btnTicker=0x7f070001;
-        public static final int btnTickerText=0x7f070003;
-        public static final int textView1=0x7f070002;
+        public static final int LinearLayout1=0x7f080001;
+        public static final int btnTicker=0x7f080002;
+        public static final int btnTickerText=0x7f080004;
+        public static final int textView1=0x7f080003;
+        public static final int widget_textview=0x7f080000;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int bitcoinwidget=0x7f030000;
+        public static final int main=0x7f030001;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
+        public static final int app_name=0x7f050000;
+        public static final int widget_text=0x7f050001;
     }
     public static final class style {
         /** 
@@ -50,10 +53,13 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
+    }
+    public static final class xml {
+        public static final int bitcoin_ticker_widget=0x7f040000;
     }
 }
